9ed989926ac453f881f8fbee1220a960ca2784d1
AzizBenAmor <azizbenamor1288@gmail.com>
2024-09-09 03:13:46 +0100

    READ ME AND SOME NEW USER

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,6 +16,20 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $user = User::create([
+            'email' => 'admin@gmail.com',
+            'name' => 'admin',
+            'password' => Hash::make('12345678'),
+        ]);
+        $user->assignRole(RoleEnum::admin()->value);
+        assignPermissionsTo($user, RoleEnum::admin()->value);
+        $user = User::create([
+            'email' => 'user@gmail.com',
+            'name' => 'User',
+            'password' => Hash::make('12345678'),
+        ]);
+        $user->assignRole(RoleEnum::user()->value);
+        assignPermissionsTo($user, RoleEnum::user()->value);
+        $user = User::create([
             'email' => 'user1@gmail.com',
             'name' => 'User1',
             'password' => Hash::make('12345678'),
